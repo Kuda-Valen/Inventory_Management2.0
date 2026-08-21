6c7f939e0c113e6acd70198c4a82b1a5fae21151
Kuda_Valen <kudamukwasi@gmail.com>
2026-08-21 23:22:34 +0200

    worked precisely on menus

--- Frontend/interface/menus.py
+++ Frontend/interface/menus.py
@@ -3,52 +3,133 @@ from services.add_product import add_product
 def manager_menu():
     while True:
         print("\n-- Manager --\n")
-        print("1. Add product")
-        print("2. Add Staff")
-        print("3. Make a Purchase")
-        print("4. View Profile")
-        print("5. Edit Product")
-        print("6. Remove Product")
-        print("7. View Products")
-        print("8. View Sales")
-        print("9. Return to Main Menu")
+        print("1. Product Menu") # -> View Products, Edit product, Remove product
+        print("2. Start Transaction")
+        print("3. Staff") # -> Add staff, view staff profiles
+        print("4. Reports") # -> View Daily reports, Employee reports, monthly reports
+        print("5. Profile")
+        print("6. Return to Main Menu")
 
         try:
             option = int(input("\nChoose an Option: "))
 
             if option == 1:
-                add_product()
+                while True:
+                    print("\n-- Products --\n")
+                    print("1. Add Product")
+                    print("2. View Products")
+                    print("3. Edit Product")
+                    print("4. Remove Product")
+                    print("5. Return to Manager Menu")
 
+                    try:
+                        option = int(input("\nChoose an Option: "))
+
+                        if option == 1:
+                            add_product()
+
+                        elif option == 2:
+                            # View products
+                            ...
+
+                        elif option == 3:
+                            # Edit product
+                            ...
+
+                        elif option == 4:
+                            # Remove Product
+                            ...
+
+                        elif option == 5:
+                            print("Returning..")
+                            break
+
+                        else:
+                            print("\nInvalid Option. Choose a Valid option!!")
+
+                    except ValueError as e:
+                        print(f"\nEncountered Input Error: {e}")
 
             elif option == 2:
-                # Add Staff
+                # Start Transaction
                 ...
 
             elif option == 3:
-                # Make a purchase
-                ...
+                while True:
+                    print("\n-- Staff --\n")
+                    print("1. Add Staff")
+                    print("2. View Staff Stats")
+                    print("3. Remove Staff")
+                    print("4. Return")
+
+                    try:
+                        option = int(input("\nChoose Option: "))
+
+                        if option == 1:
+                            # Add Staff
+                            ...
+
+                        elif option == 2:
+                            # View Staff Stats
+                            ...
+
+                        elif option == 3:
+                            # Remove Staff 
+                            ...
+
+                        elif option == 4:
+                            print("\nReturning..")
+                            break
+
+                        else:
+                            print("\nInvalid option. Choose a Valid Option!!")
+
+                    except ValueError as e:
+                        print(f"\nEncountered Input Error: {e}")
 
             elif option == 4:
-                # View Profile (either user/other users)
-                ...
+                while True:
+                    print("\n-- Reports --\n")
+                    print("1. Daily Report")
+                    print("2. Staff Report")
+                    print("3. Monthly Report")
+                    print("4. Product Report")
+                    print("5. Return")
+
+                    try:
+                        option = int(input("\nChoose Option: "))
+
+                        if option == 1:
+                            # Daily Report
+                            ...
+
+                        elif option == 2:
+                            # Staff Report
+                            ...
+
+                        elif option == 3:
+                            # Monthly Report
+                            ...
+
+                        elif option == 4:
+                            # Product Report
+                            ...
+
+                        elif option == 5:
+                            print("\nReturning...")
+                            break
+
+                        else:
+                            print("\nInvalid Option. Choose a Valid Option!!")
+
+                    except ValueError as e:
+                        print(f"\nEncontered Input Error: {e}")
 
             elif option == 5:
-                # Edit product
+                # View Profile
                 ...
 
             elif option == 6:
-                # Remove product
-                ...
-
-            elif option == 7:
-                # View Products
-                ...
-
-            elif option == 8:
-                # View Sales
-                ...
-
-            elif option == 9:
                 print("Returning to Main menu")
                 break
 
@@ -62,8 +143,8 @@ def cashier_menu():
     while True:
         print("\n-- Staff --\n")
         print("1. View Products")
-        print("2. Make a Purchase")
-        print("3. View User Profile")
+        print("2. Start Transaction")
+        print("3. Profile")
         print("4. Return to Main Menu")
 
         try:
